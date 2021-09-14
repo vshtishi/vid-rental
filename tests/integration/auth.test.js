6842920320a8c71d4eda9f01bbcba9539e012d16
OXA <vjolashtishi@gmail.com>
2021-09-14 16:33:21 +0200

    Testing Auth Middleware

---
+++ tests/integration/auth.test.js
@@ -0,0 +1,37 @@
+const request = require('supertest');
+const { User } = require('../../models/user');
+const { Genre } = require('../../models/genre');
+
+let server;
+
+describe('auth middleware', () => {
+    beforeEach(() => {
+        server = require('../../index');
+    });
+    afterEach(async () => {
+        await Genre.remove({});
+        server.close();
+    });
+
+   
+    let token;
+    
+    const exec = () => {
+        return request(server).post('/api/genres').set('x-auth-token', token).send({ name: 'genre' });
+    }
+
+
+    beforeEach(() => {
+        token = new User().generateAuthToken();
+    });
+
+   
+
+
+    it('should return 401 if no token is provided', async () => {
+        token = '';
+        const res = await exec();
+
+        expect(res.status).toBe(401);
+    });
+})
