3cf359aa8323f7cf0a4095a78d1b03021a4ebc3d
OXA <vjolashtishi@gmail.com>
2021-09-14 10:10:55 +0200

    Integration Test for Genres Endpoint

--- routes/genres.js
+++ routes/genres.js
@@ -50,6 +50,9 @@ router.put('/:id', auth, async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        return res.status(404).send('Invalid ID');
+    }
     const genre = await Genre.findById(req.params.id);
 
     if (!genre) {
