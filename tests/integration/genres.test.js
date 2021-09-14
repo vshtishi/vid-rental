f8b4e051aa4dc9450f9d8c521429bf1572833fa6
OXA <vjolashtishi@gmail.com>
2021-09-14 12:20:01 +0200

    Added Additional Integration Tests for Genres

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -72,6 +72,25 @@ describe('/api/genres', () => {
             expect(res.status).toBe(400);
 
         });
+
+        it('should save the genre if it is valid', async () => {
+            const token = new User().generateAuthToken();
+
+            const res = await request(server).post('/api/genres').set('x-auth-token', token).send({ name: 'genre' });
+
+            const genre = await Genre.find({ name: 'genre' });
+
+            expect(genre).not.toBeNull();
+        });
+
+        it('should return the genre if it is valid', async () => {
+            const token = new User().generateAuthToken();
+
+            const res = await request(server).post('/api/genres').set('x-auth-token', token).send({ name: 'genre' });
+
+            expect(res.body).toHaveProperty('_id');
+            expect(res.body).toHaveProperty('name', 'genre');
+        });
     })
 });
 
